0f1fc5f97e943e4f0806e328ee8d97cc2a2c8ed2
Blarc <jakob.malezic@gmail.com>
2023-11-06 20:19:53 +0100

    feat(backend): add OAuth2 token verifier

--- backend/controllers/user.go
+++ backend/controllers/user.go
@@ -5,11 +5,7 @@ import (
 	"github.com/Blarc/advent-of-code-bingo/models"
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"github.com/google/go-github/v56/github"
 	"golang.org/x/oauth2"
-	goauth "google.golang.org/api/oauth2/v2"
-	"google.golang.org/api/option"
-	"log"
 	"net/http"
 )
 
@@ -20,68 +16,71 @@ func init() {
 func LogInUserGitHub(c *gin.Context, conf *oauth2.Config) {
 
 	accessToken := c.MustGet("token").(oauth2.Token)
-	githubClient := github.NewClient(conf.Client(c, &accessToken))
-	githubUserData, _, err := githubClient.Users.Get(c, "")
-	if err != nil {
-		return
-	}
-
-	var user models.User
-	models.DB.FirstOrCreate(&user, models.User{
-		OAuthID:   *githubUserData.ID,
-		Name:      *githubUserData.Name,
-		AvatarURL: *githubUserData.AvatarURL,
-		GitHubURL: *githubUserData.HTMLURL,
-	})
-
-	log.Printf("Saving user to session: %v", user)
-	session := sessions.Default(c)
-	session.Set("user", user)
-	if err := session.Save(); err != nil {
-		log.Printf("Failed to save session: %v", err)
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-	c.Redirect(http.StatusMovedPermanently, "/login")
+	//githubClient := github.NewClient(conf.Client(c, &accessToken))
+	//githubUserData, _, err := githubClient.Users.Get(c, "")
+	//if err != nil {
+	//	return
+	//}
+	//
+	//var user models.User
+	//models.DB.FirstOrCreate(&user, models.User{
+	//	OAuthID:   *githubUserData.ID,
+	//	Name:      *githubUserData.Name,
+	//	AvatarURL: *githubUserData.AvatarURL,
+	//	GitHubURL: *githubUserData.HTMLURL,
+	//})
+	//
+	//log.Printf("Saving user to session: %v", user)
+	//session := sessions.Default(c)
+	//session.Set("user", user)
+	//if err := session.Save(); err != nil {
+	//	log.Printf("Failed to save session: %v", err)
+	//	c.AbortWithError(http.StatusInternalServerError, err)
+	//	return
+	//}
+	//c.Redirect(http.StatusMovedPermanently, "/login")
+	c.JSON(http.StatusOK, accessToken)
 }
 
 func LogInUserReddit(c *gin.Context) {
-	//accessToken := c.MustGet("token").(oauth2.Token)
-	c.Redirect(http.StatusMovedPermanently, "/login")
+	accessToken := c.MustGet("token").(oauth2.Token)
+	//c.Redirect(http.StatusMovedPermanently, "/login")
+	c.JSON(http.StatusOK, accessToken)
 }
 
 func LogInUserGoogle(c *gin.Context, conf *oauth2.Config) {
 	accessToken := c.MustGet("token").(oauth2.Token)
 
-	googleService, err := goauth.NewService(c, option.WithTokenSource(conf.TokenSource(c, &accessToken)))
-	if err != nil {
-		log.Printf("Failed to create oauth service: %v", err)
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-
-	userInfo, err := googleService.Userinfo.Get().Do()
-	if err != nil {
-		log.Printf("Failed to get userinfo for user: %v", err)
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-
-	var user models.User
-	models.DB.FirstOrCreate(&user, models.User{
-		Name:      userInfo.Name,
-		AvatarURL: userInfo.Picture,
-	})
-
-	log.Printf("Saving user to session: %v", user)
-	session := sessions.Default(c)
-	session.Set("user", user)
-	if err := session.Save(); err != nil {
-		log.Printf("Failed to save session: %v", err)
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-	c.Redirect(http.StatusMovedPermanently, "/login")
+	//googleService, err := goauth.NewService(c, option.WithTokenSource(conf.TokenSource(c, &accessToken)))
+	//if err != nil {
+	//	log.Printf("Failed to create oauth service: %v", err)
+	//	c.AbortWithError(http.StatusInternalServerError, err)
+	//	return
+	//}
+	//
+	//userInfo, err := googleService.Userinfo.Get().Do()
+	//if err != nil {
+	//	log.Printf("Failed to get userinfo for user: %v", err)
+	//	c.AbortWithError(http.StatusInternalServerError, err)
+	//	return
+	//}
+	//
+	//var user models.User
+	//models.DB.FirstOrCreate(&user, models.User{
+	//	Name:      userInfo.Name,
+	//	AvatarURL: userInfo.Picture,
+	//})
+	//
+	//log.Printf("Saving user to session: %v", user)
+	//session := sessions.Default(c)
+	//session.Set("user", user)
+	//if err := session.Save(); err != nil {
+	//	log.Printf("Failed to save session: %v", err)
+	//	c.AbortWithError(http.StatusInternalServerError, err)
+	//	return
+	//}
+	//c.Redirect(http.StatusMovedPermanently, "/login")
+	c.JSON(http.StatusOK, accessToken)
 }
 
 func FindMe(c *gin.Context) {
--- backend/gin-oauth2/gin_oauth2.go
+++ backend/gin-oauth2/gin_oauth2.go
@@ -6,17 +6,16 @@ import (
 	"encoding/base64"
 	"encoding/gob"
 	"fmt"
-	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"github.com/golang/glog"
 	"golang.org/x/oauth2"
 	"log"
 	"net/http"
+	"time"
 )
 
 const (
-	stateKey  = "state"
-	sessionID = "gin_oauth2_session"
+	stateKey = "state"
+	cookieID = "gin_oauth2_cookie"
 )
 
 type GinOAuth2 struct {
@@ -27,75 +26,53 @@ func init() {
 	gob.Register(oauth2.Token{})
 }
 
-func (ginOauth2 *GinOAuth2) Auth() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-
-		// Check if token is already in session and set it in context if it is
-		session := sessions.Default(ctx)
-		existingSession := session.Get(sessionID)
-		if token, ok := existingSession.(oauth2.Token); ok {
-			ctx.Set("token", token)
-			ctx.Next()
-			return
-		}
-
-		// Check if state is valid - this prevents CSRF attacks
-		retrievedState := session.Get(stateKey)
-		log.Println("Retrieved state: ", retrievedState)
-		if retrievedState != ctx.Query(stateKey) {
-			ctx.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s.", retrievedState))
-			return
-		}
-
-		// Exchange code for token
-		log.Println("Exchanging code for token")
-		token, err := ginOauth2.Config.Exchange(context.TODO(), ctx.Query("code"))
-		if err != nil {
-			ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("Failed to exchange code for oauth token: %w.", err))
-			return
-		}
-		// TODO: Not sure if this is needed
-		ctx.Set("token", *token)
-
-		// Save token in session
-		log.Println("Saving session")
-		session.Set(sessionID, token)
-		if err := session.Save(); err != nil {
-			log.Printf("[GIN-oauth2] Failed to save session: %v", err)
-			ctx.AbortWithError(http.StatusInternalServerError, fmt.Errorf("Failed to save session: %v.", err))
-			return
-		}
-	}
-}
-
 func (ginOauth2 *GinOAuth2) LoginRedirectHandler(ctx *gin.Context) {
-	stateValue := randToken()
-	session := sessions.Default(ctx)
-	session.Set(stateKey, stateValue)
-	session.Save()
-	ctx.Redirect(http.StatusTemporaryRedirect, ginOauth2.Config.AuthCodeURL(stateValue))
+	oauthState := generateStateOauthCookie(ctx)
+	url := ginOauth2.Config.AuthCodeURL(oauthState)
+	ctx.Redirect(http.StatusTemporaryRedirect, url)
 }
 
-func (ginOauth2 *GinOAuth2) LoginHandler(ctx *gin.Context, authProviderName string) {
-	stateValue := randToken()
-	session := sessions.Default(ctx)
-	session.Set(stateKey, stateValue)
-	session.Save()
-	log.Println(ginOauth2.Config.AuthCodeURL(stateValue))
-	ctx.Writer.Write([]byte(`
-	<html>
-	  <body>
-			<a href='` + ginOauth2.Config.AuthCodeURL(stateValue) + `'>
-				<button>Login with ` + authProviderName + `!</button>
-			</a>
-		</body>
-	</html>`))
+func (ginOauth2 *GinOAuth2) CallbackHandler(ctx *gin.Context) {
+	token := ginOauth2.GetToken(ctx)
+	ctx.JSON(http.StatusOK, token)
 }
 
-func randToken() string {
-	b := make([]byte, 32)
-	if _, err := rand.Read(b); err != nil {
-		glog.Fatalf("[Gin-OAuth] Failed to read rand: %v", err)
+func (ginOauth2 *GinOAuth2) GetToken(ctx *gin.Context) *oauth2.Token {
+	// Read oauthState from Cookie
+	oauthState, _ := ctx.Cookie(cookieID)
+
+	/*
+		AuthCodeURL receive state that is a token to protect the user from CSRF attacks.
+		You must always provide a non-empty string and validate that it matches the state
+		query parameter on your redirect callback.
+	*/
+	if ctx.Query(stateKey) != oauthState {
+		log.Println("Invalid oauth state.")
+		ctx.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s.", oauthState))
+		return nil
 	}
-	return base64.StdEncoding.EncodeToString(b)
+
+	log.Printf("Code: %v", ctx.Query("code"))
+
+	// Exchange code for token
+	log.Println("Exchanging code for token")
+	token, err := ginOauth2.Config.Exchange(context.TODO(), ctx.Query("code"))
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("Failed to exchange code for oauth token: %w.", err))
+		return nil
+	}
+
+	return token
+}
+
+func generateStateOauthCookie(ctx *gin.Context) string {
+	var expiration = time.Now().Add(365 * 24 * time.Hour)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: cookieID, Value: state, Expires: expiration}
+	http.SetCookie(ctx.Writer, &cookie)
+
+	return state
 }
