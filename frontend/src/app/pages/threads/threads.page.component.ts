e8406889b2eb06e381e1fe09a55984964a9a3d3d
Bogatejc <mark.bogataj18@gmail.com>
2023-11-13 10:07:07 +0100

    feat(frontend): updated bingo and threads view

--- frontend/src/app/pages/threads/threads.page.component.ts
+++ frontend/src/app/pages/threads/threads.page.component.ts
@@ -3,6 +3,17 @@ import {Component} from '@angular/core';
 @Component({
     standalone: true,
     selector: 'app-threads',
-    templateUrl: 'threads.page.component.html'
+    templateUrl: 'threads.page.component.html',
+    styleUrls: ['threads.page.component.scss']
 })
-export class ThreadsPageComponent {}
+export class ThreadsPageComponent {
+    public voteCounter = 0;
+
+    upVote() {
+        this.voteCounter++;
+    }
+
+    downVote() {
+        this.voteCounter--;
+    }
+}
