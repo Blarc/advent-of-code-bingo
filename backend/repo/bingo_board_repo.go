df433503204c0ed3034f768c15dc93168e9cd064
Blarc <jakob.malezic@gmail.com>
2023-11-29 15:57:15 +0100

    save

--- backend/repo/bingo_board_repo.go
+++ backend/repo/bingo_board_repo.go
@@ -7,6 +7,7 @@ import (
 
 type BingoBoardRepo interface {
 	FindBingoBoard(id string) (*models.BingoBoard, error)
+	GetBingoCardsWithCount(uuid string) ([]models.BingoCardDto, error)
 }
 
 type bingoBoardRepo struct {
@@ -24,4 +25,21 @@ func (b *bingoBoardRepo) FindBingoBoard(shortUuid string) (*models.BingoBoard, e
 		return nil, result.Error
 	}
 	return &bingoBoard, nil
+}
+
+// GetBingoCardsWithCount returns all bingo cards for a board and how many users have them
+func (b *bingoBoardRepo) GetBingoCardsWithCount(shortUuid string) ([]models.BingoCardDto, error) {
+	var bingoCards []models.BingoCardDto
+	result := models.DB.Table("bingo_cards").
+		Select("bingo_cards.id, bingo_cards.description, count(user_bingo_card.user_id) as user_count").
+		Joins("left join bingo_board_bingo_card ON bingo_board_bingo_card.bingo_card_id = bingo_cards.id").
+		Joins("left join user_bingo_card ON user_bingo_card.bingo_card_id = bingo_cards.id").
+		Group("bingo_board_bingo_card.bingo_board_id, bingo_cards.id").
+		Find(&bingoCards, "substring(bingo_board_bingo_card.bingo_board_id::text, 1, 16) = ?", shortUuid)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return bingoCards, nil
 }
